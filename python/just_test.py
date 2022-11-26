5d2e6443292098f84ee69d2f5667e4eb88fb85c3
GrubbyWang <15971290672@163.com>
2022-11-26 22:45:19 +0800

    k8s实践文件

--- python/just_test.py
+++ python/just_test.py
@@ -1,8 +1,17 @@
 import os
 import sys
+import random as rd
+
 
 __file_name = os.path.realpath(__file__)
 __cur_path = os.path.dirname(__file_name)
+
+def fun(ok):
+    # import random
+    a = rd.randint(0, 10)
+    if ok:
+        import random
+
 
 
 if __name__ == "__main__":
